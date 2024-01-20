7a9948c8c7d99edbfa3be5f26144b7746050ea35
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-01-20 21:04:27 +0200

    fix task 1

--- stack.c
+++ stack.c
@@ -60,6 +60,7 @@ void _pint(stack_t **stack, unsigned int line_number)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		free_all(stack);
+		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
 }
