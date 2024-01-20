72f1d36cb27f1f966d0b09a00a04c33a2e4972ee
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-01-20 21:33:14 +0200

    fixing tasks

--- stack2.c
+++ stack2.c
@@ -11,7 +11,7 @@ void _mul(stack_t **stack, unsigned int line_number)
 
 	if ((stkint_len(*stack)) < 2)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		free_all(stack);
 		exit(EXIT_FAILURE);
 	}
@@ -33,7 +33,7 @@ void _div(stack_t **stack, unsigned int line_number)
 
 	if ((stkint_len(*stack)) < 2)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
 		free_all(stack);
 		exit(EXIT_FAILURE);
 	}
@@ -55,7 +55,7 @@ void _mod(stack_t **stack, unsigned int line_number)
 
 	if ((stkint_len(*stack)) < 2)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
 		free_all(stack);
 		exit(EXIT_FAILURE);
 	}
